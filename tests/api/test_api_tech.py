e7e4ed96ad5ca8acfcf57ae92954f020ea9d5112
Russell006 <russell.shamim@gmail.com>
2026-02-21 17:31:57 +0100

    tekniska test och fixat conftest

---
+++ tests/api/test_api_tech.py
@@ -0,0 +1,19 @@
+from src.helpers.loan_client import LoanApiClient
+import requests
+from config import BASE_URL
+
+
+client = LoanApiClient()
+
+#"tekniskt" test mha requests-biblioteket, dÃ¤r headers, HTTP statuskod mm valideras
+def test_get_status():
+    response = client.get_loans()
+    assert response.status_code == 200
+    assert "application/json" in response.headers["Content-Type"]
+
+def test_get_without_key():
+    response = requests.get(f"{BASE_URL}/partner-loan-api")
+    assert response.status_code ==401
+
+    
+#"affÃ¤rs"-test mha requests-biblioteket, dÃ¤r del av JSON-svaret/body:n valideras
